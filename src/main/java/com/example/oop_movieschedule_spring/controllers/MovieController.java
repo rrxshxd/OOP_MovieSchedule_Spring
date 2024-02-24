b0eb686b8c65ac922f8bf5cff8bcab578b8b43bc
Rashad <230924@astanait.edu.kz>
2024-02-24 21:43:01 +0600

    Created models

---
+++ src/main/java/com/example/oop_movieschedule_spring/controllers/MovieController.java
@@ -0,0 +1,12 @@
+package com.example.oop_movieschedule_spring.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MovieController {
+    @GetMapping
+
+}
---
+++ src/main/java/com/example/oop_movieschedule_spring/models/Ticket.java
@@ -0,0 +1,20 @@
+package com.example.oop_movieschedule_spring.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name=)
+public class Ticket {
+    @Id
+    @GeneratedValue
+    private int id;
+    private int ticketId;
+    private int sessionId;
+    private String movieName;
+    private boolean isBooked;
+}
